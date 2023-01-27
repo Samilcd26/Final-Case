89f7fcd8f60973866ef51bc2f43f991c3d05c1b3
Şamil Cihat DEMİR <83214912+Samilcd26@users.noreply.github.com>
2023-01-27 23:05:44 +0300

    Delete src directory

--- src/main/java/com/FinalCase/business/config/WebSecurityConfig.java
+++
@@ -1,6 +0,0 @@
-package com.FinalCase.business.config;
-
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-
